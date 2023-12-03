55f9ae1940e706f8361296e34844e457c9906721
HunterChen <1480811935cyt@gmail.com>
2023-12-03 15:26:34 +0800

    demo for cash card

---
+++ src/main/java/com/example/cashcarddemo/CashCardDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashcarddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashCardDemoApplication.class, args);
+    }
+
+}
